a3a53795e955dc18e4a6e707cd84573896d1c2f6
SasidharBollineni <bollinenisasidhar777@gmail.com>
2026-03-08 19:46:42 +0530

    added

---
+++ src/Hari/Animal.java
@@ -0,0 +1,45 @@
+package Hari;
+
+import Hari.Animal;
+import Hari.Animal.Cat;
+
+public class Animal {
+	private String name;
+			
+	public Animal (String name) {
+		this.name = name;
+	}
+	public void eat() {
+	System.out.println(name + " is eating");
+	}
+	public void sleep() {
+		System.out.println(name + " is Sleeping");
+	}
+
+    public static class Cat extends Animal{
+	  public Cat(String name) {
+		  super(name);
+	  }
+	  public void meow() {
+		 System.out.println("The cat is meowing.");
+	  }
+    }
+	
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	   //Animal a = new Animal(null);
+	   
+		Cat mycat = new Cat("Whiskers");
+		mycat.eat();
+		mycat.sleep();
+		mycat.meow();
+	}
+
+}
+
+
+
+	
+		// TODO Auto-generated method stub
+
+
